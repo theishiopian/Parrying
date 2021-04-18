f57f09c6417e95b3778bc4afb66443af2148bc03
theishiopian <theishiopian@gmail.com>
2021-04-17 18:52:45 -0700

    Implemented check for tiered item

--- src/main/java/com/theishiopian/parrying/EventHandler.java
+++ src/main/java/com/theishiopian/parrying/EventHandler.java
@@ -2,9 +2,11 @@ package com.theishiopian.parrying;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.TieredItem;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
 public class EventHandler
@@ -19,21 +21,25 @@ public class EventHandler
             ParryingMod.LOGGER.info("parry event called");
 
             ServerPlayerEntity player = (ServerPlayerEntity) event.getEntity();
-            Entity attacker = source.getEntity();
 
-            Vector3d playerDir = player.getViewVector(1);
-            Vector3d attackerDir = attacker.position().subtract(player.position());
-
-            attackerDir = attackerDir.normalize();
-
-            double angle = playerDir.dot(attackerDir);
-
-            ParryingMod.LOGGER.info(angle);
-
-            if(angle > 0.8)
+            if(player.getMainHandItem().getItem() instanceof TieredItem)
             {
-                event.setCanceled(true);
-                ParryingMod.LOGGER.info("blocked");
+                Entity attacker = source.getEntity();
+
+                Vector3d playerDir = player.getViewVector(1);
+                Vector3d attackerDir = attacker.position().subtract(player.position());
+
+                attackerDir = attackerDir.normalize();
+
+                double angle = playerDir.dot(attackerDir);
+
+                ParryingMod.LOGGER.info(angle);
+
+                if(angle > 0.8)
+                {
+                    event.setCanceled(true);
+                    ParryingMod.LOGGER.info("blocked");
+                }
             }
         }
     }
