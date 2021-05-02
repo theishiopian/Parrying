4d43674069a9c48bf9cb2e6f3e5c520886ae1644
theishiopian <theishiopian@gmail.com>
2021-05-01 17:05:08 -0700

    -balancing pass -added parrying to trident -fixed build files

--- src/main/java/com/theishiopian/parrying/EventHandler.java
+++ src/main/java/com/theishiopian/parrying/EventHandler.java
@@ -3,6 +3,8 @@ package com.theishiopian.parrying;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.TieredItem;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
@@ -15,18 +17,16 @@ import org.apache.logging.log4j.Logger;
 public class EventHandler
 {
     static Logger log = ParryingMod.LOGGER;
+
     public static void OnAttackedEvent(LivingAttackEvent event)
     {
         DamageSource source = event.getSource();
 
-
         if(event.getEntity() instanceof ServerPlayerEntity && source instanceof EntityDamageSource)
         {
-            //ParryingMod.LOGGER.info("parry event called");
-
             PlayerEntity player = (PlayerEntity) event.getEntity();
-
-            if(player.getMainHandItem().getItem() instanceof TieredItem)
+            Item held = player.getMainHandItem().getItem();
+            if(held instanceof TieredItem || held == Items.TRIDENT)
             {
                 Entity attacker = source.getEntity();
 
@@ -37,13 +37,11 @@ public class EventHandler
 
                 double angle = playerDir.dot(attackerDirNorm);
 
-                if(angle > 0.8 && player.swinging)
+                if(angle > 0.95 && player.swinging)
                 {
                     log.info("blocked");
                     player.level.playSound(null, player.blockPosition(), SoundEvents.SHIELD_BLOCK, SoundCategory.PLAYERS, 1,1);
-                    double dirX = player.getX() - attacker.getX();
-                    double dirZ = player.getZ() - attacker.getZ();
-                    player.knockback(1, -dirX, -dirZ);
+                    player.knockback(0.33f, attackerDir.x, attackerDir.z);
                     player.hurtMarked = true;//this makes knockback work
                     event.setCanceled(true);
                 }
