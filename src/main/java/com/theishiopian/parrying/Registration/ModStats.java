b0ce3e599531388fbe0b90d24b2273f9fa5adf24
theishiopian <theishiopian@gmail.com>
2021-08-29 15:15:21 -0700

    Added chinese translation

--- src/main/java/com/theishiopian/parrying/Registration/ModStats.java
+++ src/main/java/com/theishiopian/parrying/Registration/ModStats.java
@@ -1,7 +1,6 @@
 package com.theishiopian.parrying.Registration;
 
 import com.theishiopian.parrying.ParryingMod;
-
 import net.minecraft.stats.IStatFormatter;
 import net.minecraft.stats.StatType;
 import net.minecraft.stats.Stats;
@@ -13,12 +12,14 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 @EventBusSubscriber(bus = Bus.MOD, modid = ParryingMod.MOD_ID)
-public class ModStats {
+public class ModStats
+{
 
     public static ResourceLocation parry;
 
     @SubscribeEvent
-    public static void registerStats(RegistryEvent.Register<StatType<?>> event) {
+    public static void registerStats(RegistryEvent.Register<StatType<?>> event)
+    {
         parry = new ResourceLocation(ParryingMod.MOD_ID, "parry");
         Registry.register(Registry.CUSTOM_STAT, parry, parry);
         Stats.CUSTOM.get(parry, IStatFormatter.DEFAULT);
