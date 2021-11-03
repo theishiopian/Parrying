0315d4e4d67eeb0597518049e2b5e60ae8db8bf2
theishiopian <theishiopian@gmail.com>
2021-11-02 19:12:52 -0700

    Improved item check

--- src/main/java/com/theishiopian/parrying/Mixin/DamageEnchantmentMixin.java
+++ src/main/java/com/theishiopian/parrying/Mixin/DamageEnchantmentMixin.java
@@ -1,6 +1,7 @@
 package com.theishiopian.parrying.Mixin;
 
 import com.theishiopian.parrying.Registration.ModTags;
+import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,8 @@ public class DamageEnchantmentMixin
     @Inject(at = @At("HEAD"), method = "canEnchant", cancellable = true)
     private void InjectIntoCanEnchant(ItemStack stackIn, CallbackInfoReturnable<Boolean> cir)
     {
-        if(stackIn.getItem().is(ModTags.SMITE_ITEMS))
+        DamageEnchantment e = ((DamageEnchantment)(Object)this);
+        if(stackIn.getItem().is(ModTags.SMITE_ITEMS) &&  e.type == 1)
         {
             cir.setReturnValue(true);
         }
