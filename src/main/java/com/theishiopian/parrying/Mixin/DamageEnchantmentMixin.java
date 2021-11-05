5616bdb0a8c0e7a49da82a1464cad17fdabaad6a
theishiopian <theishiopian@gmail.com>
2021-11-05 14:32:59 -0700

    All AP items can now have smite for now (holy hammers anyone?)

--- src/main/java/com/theishiopian/parrying/Mixin/DamageEnchantmentMixin.java
+++ src/main/java/com/theishiopian/parrying/Mixin/DamageEnchantmentMixin.java
@@ -1,6 +1,6 @@
 package com.theishiopian.parrying.Mixin;
 
-import com.theishiopian.parrying.Registration.ModTags;
+import com.theishiopian.parrying.Items.APItem;
 import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * Allows "smite" on maces and flails
+ * Allows "smite" on maces, hammers and flails
  */
 @Mixin(net.minecraft.enchantment.DamageEnchantment.class)
 public class DamageEnchantmentMixin
@@ -18,7 +18,7 @@ public class DamageEnchantmentMixin
     private void InjectIntoCanEnchant(ItemStack stackIn, CallbackInfoReturnable<Boolean> cir)
     {
         DamageEnchantment e = ((DamageEnchantment)(Object)this);
-        if(stackIn.getItem().is(ModTags.SMITE_ITEMS) &&  e.type == 1)
+        if(stackIn.getItem() instanceof APItem && e.type == 1)
         {
             cir.setReturnValue(true);
         }
