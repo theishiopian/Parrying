5cd4020bddc8fd3dc673fecdcd2a3c3ae6ea3ebe
theishiopian <theishiopian@gmail.com>
2022-01-09 00:52:15 -0800

    Scoped crossbow now has custom model -todo: look into a custom multi model system for better scope -implemented mixin to properly animate charge from first person textures may need tweaking to make arrow more visible

--- src/main/java/com/theishiopian/parrying/Mixin/ItemInHandRendererMixin.java
+++ src/main/java/com/theishiopian/parrying/Mixin/ItemInHandRendererMixin.java
@@ -1,16 +1,20 @@
 package com.theishiopian.parrying.Mixin;
 
 import com.theishiopian.parrying.Mechanics.DualWielding;
+import com.theishiopian.parrying.Registration.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /*
-This Mixin is used to allow the off hand to swing and animate just like the main hand. This is used for dual wielding.
+This Mixin is used to allow the offhand to swing and animate just like the main hand. This is used for dual wielding.
  */
 @Mixin(ItemInHandRenderer.class)
 public abstract class ItemInHandRendererMixin
@@ -28,5 +32,11 @@ public abstract class ItemInHandRendererMixin
         float f = player.getAttackStrengthScale(1);
         //Debug.log("you better not be on a server here");
         return (DualWielding.IsDualWielding(player) ? ((!reEquip) ? f * f * f : 0) : ((!reEquip) ? 1 : 0)) - ((ItemInHandRendererAccessor)(thisRenderer)).getOffHandHeight();
+    }
+
+    @Redirect(method = {"renderArmWithItem"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z", ordinal = 1))
+    private boolean RedirectIsCrossbow(ItemStack instance, Item pItem)
+    {
+        return instance.is(Items.CROSSBOW) || instance.is(ModItems.SCOPED_CROSSBOW.get());
     }
 }
