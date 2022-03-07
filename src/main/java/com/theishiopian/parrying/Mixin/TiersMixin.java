57ff558f2a3d08aac3b7d57944a982103bdfc36a
Andrew Morin <theishiopian@gmail.com>
2022-03-07 08:22:58 -0800

    Buffed gold tools

---
+++ src/main/java/com/theishiopian/parrying/Mixin/TiersMixin.java
@@ -0,0 +1,29 @@
+package com.theishiopian.parrying.Mixin;
+
+import net.minecraft.world.item.Tiers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Tiers.class)
+public class TiersMixin
+{
+    @Inject(method = "getUses", at = @At("HEAD"), cancellable = true)
+    private void InjectIntoGetUses(CallbackInfoReturnable<Integer> cir)
+    {
+        if(Tiers.class.cast(this) == Tiers.GOLD)
+        {
+            cir.setReturnValue(180);
+        }
+    }
+
+    @Inject(method = "getAttackDamageBonus", at = @At("HEAD"), cancellable = true)
+    private void InjectIntoGetDamage(CallbackInfoReturnable<Float> cir)
+    {
+        if(Tiers.class.cast(this) == Tiers.GOLD)
+        {
+            cir.setReturnValue(2f);
+        }
+    }
+}
