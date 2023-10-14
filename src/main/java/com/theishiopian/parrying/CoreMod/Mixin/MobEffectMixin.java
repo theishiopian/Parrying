ac566f1dc2df40fb193cf5aff6574784a7fb7979
Andrew Morin <theishiopian@gmail.com>
2023-10-14 15:37:26 -0700

    Buffed regeneration -now ticks once per second regardless of amp -now heals more depending on amplifier

--- src/main/java/com/theishiopian/parrying/CoreMod/Mixin/MobEffectMixin.java
+++ src/main/java/com/theishiopian/parrying/CoreMod/Mixin/MobEffectMixin.java
@@ -3,6 +3,7 @@ package com.theishiopian.parrying.CoreMod.Mixin;
 import com.theishiopian.parrying.Config.Config;
 import com.theishiopian.parrying.Registration.ModEffects;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEffect.class)
 public class MobEffectMixin
@@ -28,6 +30,17 @@ public class MobEffectMixin
     }
 
     @Inject(method = "applyEffectTick", at = @At(value = "HEAD"))
+    private void ModifyRegenValue(LivingEntity pLivingEntity, int pAmplifier, CallbackInfo ci)
+    {
+        var effect = (MobEffect)(Object)this;
+
+        if(effect == MobEffects.REGENERATION)
+        {
+            pLivingEntity.heal(pAmplifier + 1);
+        }
+    }
+
+    @Inject(method = "applyEffectTick", at = @At(value = "HEAD"))
     private void DoSustenance(LivingEntity pLivingEntity, int pAmplifier, CallbackInfo ci)
     {
         var effect = (MobEffect)(Object)this;
@@ -36,6 +49,17 @@ public class MobEffectMixin
         {
             player.getFoodData().setFoodLevel(20);
             player.getFoodData().setSaturation(20);
+        }
+    }
+
+    @Inject(method = "isDurationEffectTick", at = @At(value = "HEAD"), cancellable = true)
+    private void BuffRegen(int pDuration, int pAmplifier, CallbackInfoReturnable<Boolean> cir)
+    {
+        var effect = (MobEffect)(Object)this;
+
+        if (effect == MobEffects.REGENERATION)
+        {
+            cir.setReturnValue(pDuration % 20 == 0);
         }
     }
 }
