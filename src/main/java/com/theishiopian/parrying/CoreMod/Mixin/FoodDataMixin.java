b75b767e44f48d6da3cf73548e28a69ef4b8ed21
Andrew Morin <theishiopian@gmail.com>
2023-10-15 19:34:21 -0700

    Stuffed sets saturation drain and hunger drain to 0 -added MixinExtras -update Swarmed icon

--- src/main/java/com/theishiopian/parrying/CoreMod/Mixin/FoodDataMixin.java
+++ src/main/java/com/theishiopian/parrying/CoreMod/Mixin/FoodDataMixin.java
@@ -1,6 +1,10 @@
 package com.theishiopian.parrying.CoreMod.Mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.theishiopian.parrying.Config.Config;
+import com.theishiopian.parrying.Registration.ModEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -13,5 +17,17 @@ public class FoodDataMixin
     private float ModifySatHeal(float constant)//disables sat healing
     {
         return Config.noSatHeal.get() ? Integer.MAX_VALUE : 0;
+    }
+
+    @ModifyConstant(method = "tick", constant = @Constant(floatValue = 1.0f, ordinal = 0))
+    private float ModifySatDrain(float constant, @Local LocalRef<Player> player)
+    {
+        return player.get().hasEffect(ModEffects.STUFFED.get()) ? 0 : 1;
+    }
+
+    @ModifyConstant(method = "tick", constant = @Constant(intValue = 1, ordinal = 0))
+    private int ModifyHungerDrain(int constant, @Local LocalRef<Player> player)
+    {
+        return player.get().hasEffect(ModEffects.STUFFED.get()) ? 0 : 1;
     }
 }
