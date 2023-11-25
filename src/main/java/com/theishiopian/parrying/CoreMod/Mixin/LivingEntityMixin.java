301bbad433dd58eb4ee5cf73f62d51c8db3e6b22
Andrew Morin <theishiopian@gmail.com>
2023-11-24 19:40:59 -0800

    remove unused shadow

--- src/main/java/com/theishiopian/parrying/CoreMod/Mixin/LivingEntityMixin.java
+++ src/main/java/com/theishiopian/parrying/CoreMod/Mixin/LivingEntityMixin.java
@@ -4,7 +4,6 @@ import com.theishiopian.parrying.CoreMod.Hooks.LivingEntityHooks;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,8 +14,6 @@ import java.util.Optional;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
 {
-    @Shadow private float speed;
-
     @Inject(method = "checkTotemDeathProtection", at = @At("HEAD"), cancellable = true)
     private void InjectIntoDeathProtectionCheck(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir)
     {
