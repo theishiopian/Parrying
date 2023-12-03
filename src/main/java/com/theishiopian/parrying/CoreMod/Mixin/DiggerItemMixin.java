3b876e09bdd95afc4717a57ab3c9cc571ee89c8c
Andrew Morin <theishiopian@gmail.com>
2023-12-02 20:37:28 -0800

    Axes no longer take increased durability damage from attacking entities (for real this time)

--- src/main/java/com/theishiopian/parrying/CoreMod/Mixin/DiggerItemMixin.java
+++ src/main/java/com/theishiopian/parrying/CoreMod/Mixin/DiggerItemMixin.java
@@ -1,19 +1,25 @@
 package com.theishiopian.parrying.CoreMod.Mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DiggerItem.class)
 public class DiggerItemMixin
 {
-    @ModifyArg(method = "hurtEnemy", at = @org.spongepowered.asm.mixin.injection.At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V"))
-    private int ModifyDamage(int pAmount, @Local ItemStack pStack, @Local LivingEntity pTarget)
+    @Inject(method = "hurtEnemy", at = @At("HEAD"), cancellable = true)
+    private void HurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker, CallbackInfoReturnable<Boolean> cir)
     {
-        return pStack.getItem() instanceof AxeItem ? 1 : 2;
+        pStack.hurtAndBreak(pStack.getItem() instanceof AxeItem ? 1 : 2, pAttacker, (entity) -> {
+            entity.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+
+        cir.setReturnValue(true);
     }
 }
